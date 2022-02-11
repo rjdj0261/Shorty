c3c55d77d426b946d50f1e5aac3a2da003651095
rjdj0261 <jhaverireyaansh15@gmail.com>
2022-02-11 15:37:52 +0530

    initial commit

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def fav():
     return flask.send_from_directory("./docs/website_files", "open-graph.ico")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.getenv("PORT"), debug=True)
+    application.run(host='0.0.0.0', port=os.getenv("PORT"), debug=True)
