7c92d6c7b500c77eb13b08d5b466e91e52b4b3e8
rjdj0261 <jhaverireyaansh15@gmail.com>
2022-02-11 15:24:20 +0530

    initial commit

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import os
 
 load_dotenv()
 
+import sys
+from os.path import abspath, dirname
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_URL"),
     traces_sample_rate=0.2,
