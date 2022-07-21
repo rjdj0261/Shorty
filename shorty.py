d78190e58ee67a09ac54475eef7361fee4db04cc
Reyaansh Jhaveri <jhaverireyaansh15@gmail.com>
2022-07-21 18:10:08 +0530

    Deleting Donate Command

--- shorty.py
+++ shorty.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#? Imports
+# ? Imports
 
 import logging
 import os
@@ -18,24 +18,22 @@ from discord import Embed
 from discord.ext import commands
 from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
+from firebase_dynamic_links import dynamic_link_builder
+import requests
+import json
 
-#? Uncomment If You Are Not Running On Heroku
-#? If You Are On Heroku Add The Environment Variables From Heroku Config Vars
 
-# from dotenv import load_dotenv
-
-# load_dotenv
-
-#? Initiate Sentry
+# ? Initiate Sentry
 
 sentry_sdk.init(
-    os.getenv("SENTRY_URL"),
+    "https://5a2f4314d5e64816871afc04b5237c13@o574256.ingest.sentry.io/5744939",
     traces_sample_rate=1.0,
     release="shorty@1.0.0",
     sample_rate=0.25
 )
 
-#? Shorten The Output Of Bytes
+# ? Shorten The Output Of Bytes
+
 
 def get_size(bytes, suffix="B"):
     """
@@ -51,72 +49,140 @@ def get_size(bytes, suffix="B"):
         bytes /= factor
 
 
-#? Shortener Creator Class
+# ? Shortener Creator Class
 
 class shortners:
     def __init__(self):
         self.isgd = pyshorteners.Shortener().isgd
-        self.nullpointer = pyshorteners.Shortener(domain="https://0x0.st").nullpointer
-        self.bitly = pyshorteners.Shortener(api_key=os.getenv("BITLY_API_KEY")).bitly
+        self.nullpointer = pyshorteners.Shortener(
+            domain="https://0x0.st").nullpointer
+        self.bitly = pyshorteners.Shortener(
+            api_key="eabb226f5855deb8fe02998123605a439f47d9b4").bitly
         self.chilpit = pyshorteners.Shortener().chilpit
         self.clckru = pyshorteners.Shortener().clckru
-        self.cuttly = pyshorteners.Shortener(api_key=os.getenv("CUTTLY_API_KEY")).cuttly
+        self.cuttly = pyshorteners.Shortener(
+            api_key="b82be139ae1d0c408a1f44d31b48dc4f5cebf").cuttly
         self.dagd = pyshorteners.Shortener().dagd
         self.osdb = pyshorteners.Shortener().osdb
         self.shortcm = pyshorteners.Shortener(
-            api_key=os.getenv("SHORTCM_API_KEY"), domain="3adu.short.gy"
+            api_key="0b36dd07-d05da0b5-67052811-e4e65108", domain="3adu.short.gy"
         ).shortcm
         self.tinyurl = pyshorteners.Shortener().tinyurl
         self.adfly = pyshorteners.Shortener(
-            api_key=os.getenv("ADFLY_API_KEY"),
-            user_id=os.getenv("ADFLY_USER_ID"),
+            api_key="d29be96973c37752a43f1f26e1f79d08",
+            user_id="24827485",
             domain="test.us",
             group_id=12,
             type="int",
         )
+        self.firebase = dynamic_link_builder(
+            api_key="AIzaSyDsi_Uumfc_Fvkjd0MC7JM01o-otzzTeRg")
+
+    def req(self, api, link):
+        if api == "exeio":
+            try:
+                req = requests.get(
+                    f"https://exe.io/api?api=afe5d717ef19304362234cf6f6b3d934ab0d1a07&url={link}").json()["shortenedUrl"]
+                return req
+            except:
+                return "AN ERROR OCCURED"
+        elif api == "gplinks":
+            try:
+                req = requests.get(
+                    f"https://gplinks.in/api?api=eeebc0507eb61fbd079041825d73a5ddb80a7a20&url={link}").json()["shortenedUrl"]
+                return req
+            except:
+                return "AN ERROR OCCURED"
+        elif api == "zagl":
+            try:
+                req = requests.get(f"").json()["shortenedUrl"]
+                return req
+            except:
+                return "AN ERROR OCCURED"
+        elif api == "zagl":
+            try:
+                req = requests.get(
+                    f"https://za.gl/api?api=00240505bb426a55e0b1b57bcb0b02cb16d329d3&url={link}").json()["shortenedUrl"]
+                return req
+            except:
+                return "AN ERROR OCCURED"
+        elif api == "earn4clicks":
+            try:
+                req = requests.get(
+                    f"https://earn4clicks.in/api?api=a9fd295d6d92818d4e8db9b854ab46f167838c4f&url={link}").json()["shortenedUrl"]
+                return req
+            except:
+                return "AN ERROR OCCURED"
+        elif api == "shortest":
+            try:
+                req = json.loads(requests.put("https://api.shorte.st/v1/data/url", {"urlToShorten": link}, headers={
+                                 "public-api-token": "dfa6329d5e397972dd919233b89d8b6f"}).content)["shortenedUrl"]
+                return req
+            except:
+                return "AN ERROR OCCURED"
+        elif api == "vurl":
+            try:
+                req = requests.get(
+                    f"https://vurl.com/api.php?url={link}").text
+                return req
+            except:
+                return "AN ERROR OCCURED"
+        elif api == "rebrandly":
+            linkRequest = {
+                "destination": link, "domain": {"fullName": "rebrand.ly"}}
+            requestHeaders = {
+                "Content-type": "application/json",
+                "apikey": "6053520de6be4edc9df4a55a95fc2949",
+            }
+            req = requests.post("https://api.rebrandly.com/v1/links",
+                                data=json.dumps(linkRequest),
+                                headers=requestHeaders)
+            if (req.status_code == requests.codes.ok):
+                newurl = r.json()
+                return newurl
+            else:
+                return "AN ERROR OCCURED"
 
 
-#? Initiate Bot
+# ? Initiate Bot
 bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or(os.getenv("BOT_PREFIX")),
+    command_prefix=commands.when_mentioned_or("?"),
     help_command=None,
 )
 
-#? Initiate Jishaku
-bot.load_extension("jishaku")
-
-#? Initate URLValidator
+# ? Initate URLValidator
 urlvalidator = URLValidator()
 
-#? Initiate Shorteners
+# ? Initiate Shorteners
 shortner = shortners()
 
-#? Initiate Statcord
-statcord_key = os.getenv("STATCORD_KEY")
+# ? Initiate Statcord
+statcord_key = "statcord.com-l6MDyFBxKPRCJESE2DTm"
 api = statcord.Client(bot, statcord_key)
 api.start_loop()
 
-#? Initiate Logger
+# ? Initiate Logger
 logger = logging.getLogger(__name__)
 
-#? Create handlers
+# ? Create handlers
 c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler(os.getenv("LOG_FILE"))
+f_handler = logging.FileHandler("bot.log")
 c_handler.setLevel(logging.WARNING)
 f_handler.setLevel(logging.ERROR)
 
-#? Create formatters and add it to handlers
+# ? Create formatters and add it to handlers
 c_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
-f_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+f_format = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 c_handler.setFormatter(c_format)
 f_handler.setFormatter(f_format)
 
-#? Add handlers to the logger
+# ? Add handlers to the logger
 logger.addHandler(c_handler)
 logger.addHandler(f_handler)
 
 
-#? Error Handler
+# ? Error Handler
 
 @bot.event
 async def on_command_error(ctx: commands.Context, error: commands.CommandError):
@@ -132,14 +198,14 @@ async def on_command_error(ctx: commands.Context, error: commands.CommandError):
         return
     else:
         message = "Oh no! Something went wrong while running the command!"
-        await bot.get_channel(os.getenv("ERROR_CHANNEL")).send(error)
+        await bot.get_channel(940164006998589470).send(error)
         logger.error(error)
 
     await ctx.send(message, delete_after=5)
     await ctx.message.delete(delay=5)
 
 
-#? Discord.py Ready Event
+# ? Discord.py Ready Event
 
 @bot.event
 async def on_ready():
@@ -173,7 +239,8 @@ async def on_ready():
     print("=" * 40, "Boot Time", "=" * 40)
     boot_time_timestamp = psutil.boot_time()
     bt = datetime.fromtimestamp(boot_time_timestamp)
-    print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
+    print(
+        f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
 
     # let's print CPU information
     print("=" * 40, "CPU Info", "=" * 40)
@@ -199,7 +266,7 @@ async def on_ready():
     logger.warning("We have logged in as {0.user}".format(bot))
 
 
-#? Discord.py Guild Join Event
+# ? Discord.py Guild Join Event
 
 @bot.event
 async def on_guild_join(guild):
@@ -211,7 +278,7 @@ async def on_guild_join(guild):
     )
 
 
-#? Discord.py Guild Remove Event
+# ? Discord.py Guild Remove Event
 
 @bot.event
 async def on_guild_remove(guild):
@@ -223,14 +290,14 @@ async def on_guild_remove(guild):
     )
 
 
-#? Discord.py on_command Event For Statcord.py
+# ? Discord.py on_command Event For Statcord.py
 
 @bot.event
 async def on_command(ctx):
     api.command_run(ctx)
 
 
-#? Adfly Command
+# ? Adfly Command
 
 @commands.cooldown(2, 30, commands.BucketType.user)
 @bot.command()
@@ -245,7 +312,27 @@ async def adfly(ctx, link):
         await ctx.send("Please Input A Valid Link!")
 
 
-#? Nullpointer Command
+# ? Firebase Command
+
+@commands.cooldown(2, 30, commands.BucketType.user)
+@bot.command()
+async def firebase(ctx, link):
+    try:
+        urlvalidator(link)
+        options = {
+            'link': link,
+            'apn': 'com.example.android',
+            'ibi': 'com.example.ios'
+        }
+        embed = Embed(title=shortner.firebase.generate_short_link(app_code='pocketurl', **options), color=0x0055FF).set_footer(
+            text="Requested By " + ctx.author.name
+        )
+        await ctx.send(embed=embed)
+    except ValidationError:
+        await ctx.send("Please Input A Valid Link!")
+
+
+# ? Nullpointer Command
 
 @commands.cooldown(2, 30, commands.BucketType.user)
 @bot.command()
@@ -260,7 +347,112 @@ async def nullpointer(ctx, link):
         await ctx.send("Please Input A Valid Link!")
 
 
-#? Isgd Command
+# ? Exeio Command
+
+@commands.cooldown(2, 30, commands.BucketType.user)
+@bot.command()
+async def exeio(ctx, link):
+    try:
+        urlvalidator(link)
+        embed = Embed(
+            title=shortner.req("exeio", link), color=0x0055FF
+        ).set_footer(text="Requested By " + ctx.author.name)
+        await ctx.send(embed=embed)
+    except ValidationError:
+        await ctx.send("Please Input A Valid Link!")
+
+
+# ? Vurl Command
+
+@commands.cooldown(2, 30, commands.BucketType.user)
+@bot.command()
+async def vurl(ctx, link):
+    try:
+        urlvalidator(link)
+        embed = Embed(
+            title=shortner.req("vurl", link), color=0x0055FF
+        ).set_footer(text="Requested By " + ctx.author.name)
+        await ctx.send(embed=embed)
+    except ValidationError:
+        await ctx.send("Please Input A Valid Link!")
+
+
+# ? Rebrandly Command
+
+@commands.cooldown(2, 30, commands.BucketType.user)
+@bot.command()
+async def rebrandly(ctx, link):
+    try:
+        urlvalidator(link)
+        embed = Embed(
+            title=shortner.req("rebrandly", link), color=0x0055FF
+        ).set_footer(text="Requested By " + ctx.author.name)
+        await ctx.send(embed=embed)
+    except ValidationError:
+        await ctx.send("Please Input A Valid Link!")
+
+
+# ? Shortest Command
+
+@commands.cooldown(2, 30, commands.BucketType.user)
+@bot.command()
+async def shortest(ctx, link):
+    try:
+        urlvalidator(link)
+        embed = Embed(
+            title=shortner.req("shortest", link), color=0x0055FF
+        ).set_footer(text="Requested By " + ctx.author.name)
+        await ctx.send(embed=embed)
+    except ValidationError:
+        await ctx.send("Please Input A Valid Link!")
+
+
+# ? Earn4clicks Command
+
+@commands.cooldown(2, 30, commands.BucketType.user)
+@bot.command()
+async def earn4clicks(ctx, link):
+    try:
+        urlvalidator(link)
+        embed = Embed(
+            title=shortner.req("earn4clicks", link), color=0x0055FF
+        ).set_footer(text="Requested By " + ctx.author.name)
+        await ctx.send(embed=embed)
+    except ValidationError:
+        await ctx.send("Please Input A Valid Link!")
+
+
+# ? Gplinks Command
+
+@commands.cooldown(2, 30, commands.BucketType.user)
+@bot.command()
+async def gplinks(ctx, link):
+    try:
+        urlvalidator(link)
+        embed = Embed(
+            title=shortner.req("gplinks", link), color=0x0055FF
+        ).set_footer(text="Requested By " + ctx.author.name)
+        await ctx.send(embed=embed)
+    except ValidationError:
+        await ctx.send("Please Input A Valid Link!")
+
+
+# ? Zagl Command
+
+@commands.cooldown(2, 30, commands.BucketType.user)
+@bot.command()
+async def zagl(ctx, link):
+    try:
+        urlvalidator(link)
+        embed = Embed(
+            title=shortner.req("zagl", link), color=0x0055FF
+        ).set_footer(text="Requested By " + ctx.author.name)
+        await ctx.send(embed=embed)
+    except ValidationError:
+        await ctx.send("Please Input A Valid Link!")
+
+
+# ? Isgd Command
 
 @commands.cooldown(2, 30, commands.BucketType.user)
 @bot.command()
@@ -275,7 +467,7 @@ async def isgd(ctx, link):
         await ctx.send("Please Input A Valid Link!")
 
 
-#? Bitly Command
+# ? Bitly Command
 
 @commands.cooldown(2, 30, commands.BucketType.user)
 @bot.command()
@@ -290,7 +482,7 @@ async def bitly(ctx, link):
         await ctx.send("Please Input A Valid Link!")
 
 
-#? Chilpit Command
+# ? Chilpit Command
 
 @commands.cooldown(2, 30, commands.BucketType.user)
 @bot.command()
@@ -305,7 +497,7 @@ async def chilpit(ctx, link):
         await ctx.send("Please Input A Valid Link!")
 
 
-#? Clckru Command
+# ? Clckru Command
 
 @commands.cooldown(2, 30, commands.BucketType.user)
 @bot.command()
@@ -320,7 +512,7 @@ async def clckru(ctx, link):
         await ctx.send("Please Input A Valid Link!")
 
 
-#? Cuttly Command
+# ? Cuttly Command
 
 @commands.cooldown(2, 30, commands.BucketType.user)
 @bot.command()
@@ -335,7 +527,7 @@ async def cuttly(ctx, link):
         await ctx.send("Please Input A Valid Link!")
 
 
-#? Dagd Command
+# ? Dagd Command
 
 @commands.cooldown(2, 30, commands.BucketType.user)
 @bot.command()
@@ -350,7 +542,7 @@ async def dagd(ctx, link):
         await ctx.send("Please Input A Valid Link!")
 
 
-#? Osdb Command
+# ? Osdb Command
 
 @commands.cooldown(2, 30, commands.BucketType.user)
 @bot.command()
@@ -365,7 +557,7 @@ async def osdb(ctx, link):
         await ctx.send("Please Input A Valid Link!")
 
 
-#? Shortcm Command
+# ? Shortcm Command
 
 @commands.cooldown(2, 30, commands.BucketType.user)
 @bot.command()
@@ -380,7 +572,7 @@ async def shortcm(ctx, link):
         await ctx.send("Please Input A Valid Link!")
 
 
-#? Tinyurl Command
+# ? Tinyurl Command
 
 @commands.cooldown(2, 30, commands.BucketType.user)
 @bot.command()
@@ -395,64 +587,31 @@ async def tinyurl(ctx, link):
         await ctx.send("Please Input A Valid Link!")
 
 
-#? Donate Command
-
-@bot.command()
-async def donate(ctx):
-    embed = Embed(
-        title="Donation Options",
-        description="You can donate us from the following options:",
-        color=0x0055FF,
-    )
-    embed.set_thumbnail(
-        url="https://www.goodwillaz.org/wordpress/wp-content/uploads/2018/04/5-15-1.jpg"
-    )
-    embed.add_field(
-        name="Bitcoin", value=os.getenv("BITCOIN_WALLET_ADDRESS"), inline=False
-    )
-    embed.add_field(name="Paypal", value=os.getenv("PAYPAL_LINK"), inline=False)
-    embed.add_field(name="UPI", value=os.getenv("UPI"), inline=False)
-    embed.set_footer(text="Requested by " + ctx.author.name)
-    await ctx.send(embed=embed)
-
-
-#? Invite Command
+# ? Invite Command
 
 @bot.command()
 async def invite(ctx):
     embed = Embed(
         title="Invite The Bot",
-        description=f"[{os.getenv('BOT_INVITE')}]({os.getenv('BOT_INVITE')})",
+        description=f"[https://dsc.gg/shorty](https://dsc.gg/shorty)",
         color=0x0055FF,
     ).set_footer(text="Requested By " + ctx.author.name)
     await ctx.send(embed=embed)
 
 
-#? Source Command
-
-@bot.command()
-async def source(ctx):
-    embed = Embed(
-        title="Source Code Of The Bot",
-        description="[Github](https://github.com/rjdj0261/Shorty)",
-        color=0x0055FF,
-    ).set_footer(text="Requested By " + ctx.author.name)
-    await ctx.send(embed=embed)
-
-
-#? Support Command
+# ? Support Command
 
 @bot.command()
 async def support(ctx):
     embed = Embed(
         title="Join The Support Server",
-        description=f'[{os.getenv("SUPPORT_SERVER_LINK")}]({os.getenv("SUPPORT_SERVER_LINK")})',
+        description=f'[https://dsc.gg/shorty-support](https://dsc.gg/shorty-support)',
         color=0x0055FF,
     ).set_footer(text="Requested By " + ctx.author.name)
     await ctx.send(embed=embed)
 
 
-#? Ping Command
+# ? Ping Command
 
 @bot.command()
 async def ping(ctx):
@@ -464,7 +623,7 @@ async def ping(ctx):
     await ctx.send(embed=embed)
 
 
-#? Help Command
+# ? Help Command
 
 @bot.command()
 async def help(ctx):
@@ -507,7 +666,28 @@ async def help(ctx):
         name="tinyurl", value="Shorten Your Link Using Tinyurl.", inline=False
     )
     embed.add_field(
-        name="donate", value="Donate The Developer Of The Bot", inline=False
+        name="exeio", value="Shorten Your Link Using Exeio.", inline=False
+    )
+    embed.add_field(
+        name="rebrandly", value="Shorten Your Link Using Rebrandly.", inline=False
+    )
+    embed.add_field(
+        name="gplinks", value="Shorten Your Link Using Gplinks.", inline=False
+    )
+    embed.add_field(
+        name="zagl", value="Shorten Your Link Using Zagl.", inline=False
+    )
+    embed.add_field(
+        name="shortest", value="Shorten Your Link Using Shortest.", inline=False
+    )
+    embed.add_field(
+        name="earn4clicks", value="Shorten Your Link Using Earn4clicks.", inline=False
+    )
+    embed.add_field(
+        name="vurl", value="Shorten Your Link Using Vurl.", inline=False
+    )
+    embed.add_field(
+        name="firebase", value="Shorten Your Link Using Shorty's Firebase.", inline=False
     )
     embed.add_field(
         name="invite", value="Invite The Bot To Your Server", inline=False
@@ -518,17 +698,14 @@ async def help(ctx):
     embed.add_field(
         name="ping", value="Ping Of The Bot", inline=False
     )
-    embed.add_field(
-        name="source", value="Source Code Of The Bot", inline=False
-    )
     embed.set_footer(
         text="Requested By " + ctx.author.name
     )
-    
+
     await ctx.send(embed=embed)
 
 
-#? Eval Command (Owner Only)
+# ? Eval Command (Owner Only)
 
 @bot.command(name="eval", hidden=True)
 @commands.is_owner()
@@ -558,7 +735,7 @@ async def eval_(ctx: commands.Context, *, code: str):
         )
 
 
-#? Eval Error Handler
+# ? Eval Error Handler
 
 @eval_.error
 async def eval__error(ctx, error):
@@ -572,7 +749,7 @@ async def eval__error(ctx, error):
     )
 
 
-#? Selfpurge Command (Owner Only)
+# ? Selfpurge Command (Owner Only)
 
 @bot.command(hidden=True)
 @commands.is_owner()
@@ -595,7 +772,7 @@ async def selfpurge(ctx, amount=2):
     await ctx.message.delete()
 
 
-#? Avatar Command (Owner Only)
+# ? Avatar Command (Owner Only)
 
 @bot.command(hidden=True)
 @commands.is_owner()
@@ -607,7 +784,7 @@ async def avatar(ctx, avatar_url):
             await ctx.send("Avatar changed!")
 
 
-#? Guildlist Command (Owner Only)
+# ? Guildlist Command (Owner Only)
 
 @bot.command(hidden=True)
 @commands.is_owner()
@@ -624,6 +801,6 @@ async def guildlist(ctx):
     await ctx.send(file=discord.File("guildlist.csv"))
 
 
-#? Start Bot
+# ? Start Bot
 
-bot.run(os.getenv("BOT_TOKEN"))
+bot.run("OTQwMTExODg4NjQ0MjAyNTA2.YgCpbA.fCQOHRlYHuxy-ARlGs88SLNKHIM")
